7234549c157aa7964a5e2e19bc26b0bdd4e7fd5a
udaypat <udaypatil@proton.me>
2025-02-16 03:07:16 +0530

    add user model and initialize database connection in user router

---
+++ backend/models/users.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from beanie import Document
+
+
+class Users(Document):
+    name: str
+    email: str
+    password: Optional[str] = None
+
+    class Settings:
+        name = "comments"
--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,19 +1,38 @@
-from fastapi import APIRouter
 from typing import Union
+
+from beanie import init_beanie
+from fastapi import APIRouter
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from backend.models.users import Users
 
 user_router = APIRouter()
 
 
+async def init_db():
+    client = AsyncIOMotorClient(
+        "mongodb+srv://udaypatil92:OBBarylILAhUBIPI@cluster0.kkzm9.mongodb.net/"
+    )
+    await init_beanie(database=client["sample_mflix"], document_models=[Users])
+
+
+# Register the startup event at the app level
+@user_router.on_event("startup")
+async def startup_event():
+    await init_db()
+
+
 @user_router.get("/users")
-def get_users():
-    return {"message": "List of users"}
+async def get_users():
+    result = await Users.find({"name": "Mercedes Tyler"}).to_list()
+    return result
 
 
 @user_router.get("/")
-def read_root():
+async def read_root():
     return {"Banana": "babababababbabba"}
 
 
 @user_router.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
