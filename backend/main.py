d4d46686bb02dac42c601e49229edacc2c5dffdc
Soumya Vaidya <soumyavaidya2016@gmail.com>
2025-02-15 12:17:09 -0800

    set up fast api router

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,6 @@
-from typing import Union
-
 from fastapi import FastAPI
+from routers.user import user_router
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Banana": "babababababbabba"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(user_router)
