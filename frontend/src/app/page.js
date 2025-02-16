0ba6dc6271ae2cd8ea1382e9e65ecacd3439ae8b
udaypat <udaypatil@proton.me>
2025-02-17 02:35:22 +0530

    refactor: update page components for improved readability and structure

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,17 @@
+"use client";
+
 import Link from "next/link";
 
 export default function Page() {
-  return <div><h1>Hello, I am SCAN-E</h1>
-    <br></br>
-    <button><Link href="/register">Register</Link></button>
-    <br></br>
-    <button><Link href="/login">Login</Link></button>
-    <br></br>
-    <button><Link href="/dashboard/John">Dashboard</Link></button>
+  return (
+    <div>
+      <h1>Hello, I am SCAN-E</h1>
+      <br></br>
+      <Link href="/register">Register</Link>
+      <br></br>
+      <Link href="/login">Login</Link>
+      <br></br>
+      <Link href="/dashboard/John">Dashboard</Link>
     </div>
+  );
 }
